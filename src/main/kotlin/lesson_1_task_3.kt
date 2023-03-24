8ded33fd09f98258c65cdc32151361f357fbb022
AntonStepko <stepkoanton@gmail.com>
2023-03-24 16:03:47 +0300

    KS-1-3 print data from first lesson

---
+++ src/main/kotlin/lesson_1_task_3.kt
@@ -0,0 +1,20 @@
+fun main()  {
+
+    //год полета в космос Гагарина
+    val year: Int = 1961
+
+    //Час и минуты взлета
+    var hour= 9
+    var minute = 7
+
+
+   // println(year)
+   // println("%02d".format(hour))
+   // println("%02d".format(minute))
+
+    //Часы посадки
+
+    hour = 10
+    minute = 55
+    print("%02d:%02d".format(hour,minute))
+}
