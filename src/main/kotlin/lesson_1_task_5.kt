cfbd34262e77c95a72cfbe993dc0d7daa35cf832
AntonStepko <stepkoanton@gmail.com>
2023-03-25 00:02:10 +0300

    KS-1-5 print data from first lesson

---
+++ src/main/kotlin/lesson_1_task_5.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val sec = 75                              //кол-во секунд проведенных в космосе
+
+    val min: Float = 60f                      //кол-во секунд в минуте
+    // println(sec / min)                        //кол-во минут проведенных в космосе 1.25
+
+     val minute = 1
+     val seconds = 25
+     println("%02d:%02d".format(minute,seconds))
+}
