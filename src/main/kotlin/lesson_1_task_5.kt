969de39eab92df6ed8d403c159ad0660e44a410d
AntonStepko <stepkoanton@gmail.com>
2023-03-25 13:55:23 +0300

    KS-1-5 print data from first lesson

--- src/main/kotlin/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1_task_5.kt
@@ -1,12 +1,10 @@
 fun main() {
 
-    var secInCos = 6480                          //кол-во секунд проведенных в космосе
+    val secInCos = 6480                          //кол-во секунд проведенных в космосе
 
-    val minutes = secInCos / 60             //кол-во минут проведенных в космосе
+    val minutes = secInCos / 60              //кол-во минут проведенных в космосе
+    val seconds = secInCos % 60              //кол-во секунд в остатке
 
-    //println(minutes)
 
-     secInCos = 0
-
-    println("%03d:%02d".format(minutes,secInCos))
+    println("%02d:%02d".format(minutes,seconds))
 }
