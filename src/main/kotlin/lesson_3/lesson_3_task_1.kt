80cbd4f0df5846fda00fbe3a1329f377f896d1d2
AntonStepko <stepkoanton@gmail.com>
2023-03-26 10:41:25 +0300

    KS-3-1 print data from third lesson

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+
+    val greetingDay = "Добрый день"
+    val greetingEvening = "Добрый вечер"
+    val name = "Иван"
+
+    println("$greetingDay, $name!")
+    println("$greetingEvening, $name!")
+
+
+
+}
