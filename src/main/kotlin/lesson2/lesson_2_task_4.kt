3583ff8424389b36abe48b3d3db8fc4d870e6230
AntonStepko <stepkoanton@gmail.com>
2023-03-26 09:05:45 +0300

    KS-2-5 print data from second lesson

--- src/main/kotlin/lesson2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson2/lesson_2_task_4.kt
@@ -6,8 +6,8 @@ fun main() {
     val metall: Int = 11
     val buffInProcent: Int = 20
 
-    val kristallsBuff = kristalls * 20 / 100.0
-    val metallBuff = metall * 20 / 100.0
+    val kristallsBuff = kristalls * 20 / 100
+    val metallBuff = metall * 20 / 100
 
     println("Ваш бафф кристалической руды : $kristallsBuff")
     println("Ваш бафф металлической руды : $metallBuff")
