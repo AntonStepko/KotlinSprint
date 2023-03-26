2a71b05a28d8fa38c7e4f99030be9c970743156d
AntonStepko <stepkoanton@gmail.com>
2023-03-26 17:55:55 +0300

    KS-3-2 print data from third lesson

--- src/main/kotlin/lesson_3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -2,14 +2,22 @@ package lesson_3
 
 fun main() {
 
-    val firstName = "Татьяна"
-    val patronymic = "Сергеевна"
-    val secondName1 = "Сидорова"
-    val secondName2 = "Андреева"
-    val age1 = "20 лет"
-    val age2 = "22 года"
+     val firstName: String
+    firstName = "Татьяна"
 
-    println("$secondName1 $firstName $patronymic, $age1")
-    println("$secondName2 $firstName $patronymic, $age2")
+    val patronymic: String
+    patronymic = "Сергеевна"
+
+    var secondName1: String
+    secondName1 = "Сидорова"
+
+    var secondName2: String
+    secondName2 = "Андреева"
+
+    val age1 = 20
+    val age2 = 22
+
+    println("$secondName1 $firstName $patronymic, $age1 лет")
+    println("$secondName2 $firstName $patronymic, $age2 года")
 
 }
