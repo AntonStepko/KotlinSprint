ee967a80db69c1b2975399b64404ead4b524aec0
AntonStepko <stepkoanton@gmail.com>
2023-03-27 14:45:15 +0300

    KS-3-5 print data from third lesson

---
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -0,0 +1,19 @@
+package lesson_3
+
+fun main() {
+
+    val motion: String
+    motion = "D2-D4;0"
+
+    val moveFrom: String
+    moveFrom = "D2"
+    val moveTo: String
+    moveTo = "D4"
+
+    val step: Int = 0
+
+    println(moveFrom)
+    println(moveTo)
+    println(step)
+
+}
