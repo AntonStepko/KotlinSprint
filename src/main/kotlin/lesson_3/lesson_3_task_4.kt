c4d4d8c4190c1ed40159d59570d652cc4898e60c
AntonStepko <stepkoanton@gmail.com>
2023-03-28 17:02:22 +0300

    KS-3-4 print data from third lesson

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -2,20 +2,16 @@ package lesson_3
 
 fun main() {
 
-    var moveFrom = "E"
-    var moveTo = "E"
-    var numFrom = 2
-    var numTo = 4
+    var moveFrom = "E2"
+    var moveTo = "E4"
     var step = 1
 
-    println("$moveFrom$numFrom-$moveTo$numTo; ход $step")
+    println("$moveFrom-$moveTo; ход $step")
 
-    moveFrom = "D"
-    moveTo = "D"
-    numFrom = 2
-    numTo = numFrom + 1
+    moveFrom = "D2"
+    moveTo = "D3"
     step += step
 
-    println("$moveFrom$numFrom-$moveTo$numTo; ход $step")
+    println("$moveFrom-$moveTo; ход $step")
 
 }
