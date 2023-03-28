d9a534d0e3925eac4c90c90369d4147b0a1c3022
AntonStepko <stepkoanton@gmail.com>
2023-03-28 17:27:47 +0300

    KS-3-5 print data from third lesson

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -2,15 +2,12 @@ package lesson_3
 
 fun main() {
 
-    val motion: String
-    motion = "D2-D4;0"
+    val motion = "D2-D4;0"
+    val data = motion.split("-",";")
 
-    val moveFrom: String
-    moveFrom = "D2"
-    val moveTo: String
-    moveTo = "D4"
-
-    val step: Int = 0
+    val moveFrom = data[0]
+    val moveTo = data[1]
+    val step = data[2]
 
     println(moveFrom)
     println(moveTo)
