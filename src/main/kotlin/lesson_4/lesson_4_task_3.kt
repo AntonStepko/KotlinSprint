54e31b8507ef5774d2db413f25a173eab25c59e7
AntonStepko <stepkoanton@gmail.com>
2023-03-30 13:20:59 +0300

    KS-4-3 print data from fourth lesson

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -2,19 +2,19 @@ package lesson_4
 
 fun main() {
 
-    val userWether: String = "солнечная погода"
-    val userTent: String = "тент раскрыт"
-    val userWetness: String = "влажность 20"
-    val userSeason: String = "время года зима"
+    val isWeatherSunny: Boolean = true
+    val isTentOpen: Boolean = true
+    val isWetness: Int = 20
+    val isSeasonWinter: Boolean = false
 
-    val result: Boolean = (userWether == WEATHER) && (userTent == TENT) &&
-                          (userWetness == WETNESS) && (userSeason == SEASON)
+    val result: Boolean = (isWeatherSunny == IS_WEATHER_SUNNY) && (isTentOpen == IS_TENT_OPEN) &&
+                          (isWetness == IS_WETNESS) && (isSeasonWinter == IS_SEASON_WINTER)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 
 }
 
-const val WEATHER = "солнечная погода"
-const val TENT = "тент раскрыт"
-const val WETNESS = "влажность 20"
-const val SEASON = "время года не зима"
+const val IS_WEATHER_SUNNY = true
+const val IS_TENT_OPEN = true
+const val IS_WETNESS = 20
+const val IS_SEASON_WINTER = true
