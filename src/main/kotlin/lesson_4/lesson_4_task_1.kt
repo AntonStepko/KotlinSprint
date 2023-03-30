b581e5fcffbd5fa0c7a8b1fe436250795fab1dc0
AntonStepko <stepkoanton@gmail.com>
2023-03-30 12:44:56 +0300

    KS-4-1 print data from fourth lesson

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -2,11 +2,11 @@ package lesson_4
 
 fun main() {
 
-    var bookingToday = 13
-    var bookingTomorrow = 13 - 4
+    val freeBookingToday = 0
+    val freeBookingTomorrow = 4
 
-    var comparisonResult1: Boolean = (bookingToday > NUMBER_OF_TABLES)
-    var comparisonResult2: Boolean = (bookingTomorrow < NUMBER_OF_TABLES)
+    val comparisonResult1: Boolean = (freeBookingToday <= NUMBER_OF_TABLES) && (freeBookingToday != 0)
+    val comparisonResult2: Boolean = (freeBookingTomorrow <= NUMBER_OF_TABLES)
 
     println("Доступность столиков на сегодня: $comparisonResult1\nДоступность столиков на завтра: $comparisonResult2")
 
