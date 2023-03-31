0a76fa25f2e17ed4b33a3b5a3ab9024b0d7645f8
AntonStepko <stepkoanton@gmail.com>
2023-03-31 15:45:30 +0300

    KS-4-4 print data from fourth lesson

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -3,12 +3,12 @@ package lesson_4
 fun main () {
 
     val trainingDay = 5
-    val parityDay = trainingDay % 2
+    val isDayEven = trainingDay % 2 == 0
 
-    val exerciseForHands: Boolean = (parityDay == IS_ODD_DAY)
-    val exerciseForLeg: Boolean = (parityDay == IS_PARITY_DAY)
-    val exerciseForBack: Boolean = (parityDay == IS_ODD_DAY)
-    val exerciseForPress: Boolean = (parityDay == IS_PARITY_DAY)
+    val exerciseForHands: Boolean = !isDayEven
+    val exerciseForLeg: Boolean = isDayEven
+    val exerciseForBack: Boolean = !isDayEven
+    val exerciseForPress: Boolean = isDayEven
 
     val result = """
         Упражнения для рук:    $exerciseForHands
@@ -21,6 +21,3 @@ fun main () {
    println(result)
 
 }
-
-const val IS_PARITY_DAY = 0
-const val IS_ODD_DAY = 1
