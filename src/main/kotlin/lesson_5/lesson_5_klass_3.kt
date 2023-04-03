63a9c99eee698d957894e2f555cb46ec8ad0ef8d
AntonStepko <stepkoanton@gmail.com>
2023-04-03 13:28:18 +0300

    KS-5-3 print data from fifth lesson

--- src/main/kotlin/lesson_5/lesson_5_klass_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_klass_3.kt
@@ -3,8 +3,8 @@ package lesson_5
 fun main() {
 
     println("Введите два числа от 1 до 100")
-    val inputData1 = readLine()!!.toInt()
-    val inputData2 = readLine()!!.toInt()
+    val inputData1 = readln().toInt()
+    val inputData2 = readln().toInt()
 
     if ((inputData1 == WIN_NUMBER_1 && inputData2 == WIN_NUMBER_2) ||
         (inputData1 == WIN_NUMBER_2 && inputData2 == WIN_NUMBER_1))
@@ -16,7 +16,7 @@ fun main() {
 
     else println("Неудача! Крутите барабан!")
 
-    println("Для победы нужны были цифры 12 и 63")
+    println("Для победы нужны были цифры $WIN_NUMBER_1 и $WIN_NUMBER_2")
 
 }
 const val WIN_NUMBER_1 = 12
