55b93cf089be3f7c9b1e4e0de53ee3c739a929e2
AntonStepko <stepkoanton@gmail.com>
2023-04-03 13:24:46 +0300

    KS-5-2 print data from fifth lesson

--- src/main/kotlin/lesson_5/lesson_5_task_2.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -3,12 +3,12 @@ package lesson_5
 fun main() {
 
     println("Введите год рождения")
-    val inputData = readLine()!!.toInt()
-    val userYear = NOW_YAER - inputData
+    val inputData = readln().toInt()
+    val userYear = NOW_YEAR - inputData
 
     if (userYear >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
     else if (userYear < AGE_OF_MAJORITY) println("Доступ запрещен")
 
 }
 const val AGE_OF_MAJORITY = 18
-const val NOW_YAER = 2023
+const val NOW_YEAR = 2023
