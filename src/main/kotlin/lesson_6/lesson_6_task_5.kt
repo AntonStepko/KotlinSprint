ed21229e94c11c4be382a1f758545fdd59fe9932
AntonStepko <stepkoanton@gmail.com>
2023-04-11 15:28:21 +0300

    KS-6-5 print data from sixth lesson

---
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -0,0 +1,40 @@
+package lesson_6
+
+fun main()
+{
+    println("Зарегистрируйтесь")
+    println("Придумайте логин")
+    val newUserLogin = readln()
+
+    println("Придумайте пароль")
+    val newUserPass = readln()
+
+    println("Введите логин и пароль")
+    var userLogin = readln()
+    var userPass = readln()
+    var counter = 1
+
+      while (newUserLogin != userLogin || newUserPass != userPass)
+      {
+          println("Неправильный логин или пароль, введите заново")
+          userLogin = readln()
+          userPass = readln()
+      }
+
+      while (counter <= 3)
+      {
+          counter++
+          val ranNum1 = (1..9).random()
+          val ranNum2 = (1..9).random()
+          val sum = "${ranNum1 + ranNum2}"
+          println("Докажите, что Вы не бот. Решите пример: $ranNum1 + $ranNum2 = ..")
+          val userAnswer = readln()
+
+          if (userAnswer == sum)
+          {
+              println("Добро пожаловать!")
+              return
+          } else println("Ответ неверный!")
+      }
+    println("Доступ запрещен!")
+}
