ad5065637dc705ac8c3df822e65b320b57e16e61
AntonStepko <stepkoanton@gmail.com>
2023-04-12 22:27:45 +0300

    KS-7-2 print data from seventh lesson

--- src/main/kotlin/lesson_7/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -1,15 +1,16 @@
 package lesson_7
-fun main () {
 
-    while (true){
-            val password = (1000..9999).random()
-            println("Ваш код авторизации: $password")
-            println("Введите код авторизации:")
-            val userPassword = readln().toInt()
+fun main() {
 
-            if (userPassword == password) {
-                println("Добрый день!")
-                return
-            }
+    while (true) {
+        val password = (1000..9999).random()
+        println("Ваш код авторизации: $password")
+        println("Введите код авторизации:")
+        val userPassword = readln().toInt()
+
+        if (userPassword == password) {
+            println("Добрый день!")
+            return
+        }
     }
 }
