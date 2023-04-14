791a7b412f34ed4e43f295124e3d2321ac576623
AntonStepko <stepkoanton@gmail.com>
2023-04-14 08:42:27 +0300

    KS-6-5 print data from sixth lesson

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -1,7 +1,6 @@
 package lesson_6
 
-fun main()
-{
+fun main() {
     println("Зарегистрируйтесь")
     println("Придумайте логин")
     val newUserLogin = readln()
@@ -14,15 +13,13 @@ fun main()
     var userPass = readln()
     var counter = 1
 
-      while (newUserLogin != userLogin || newUserPass != userPass)
-      {
+      while (newUserLogin != userLogin || newUserPass != userPass) {
           println("Неправильный логин или пароль, введите заново")
           userLogin = readln()
           userPass = readln()
       }
 
-      while (counter <= 3)
-      {
+      while (counter <= 3) {
           counter++
           val ranNum1 = (1..9).random()
           val ranNum2 = (1..9).random()
@@ -30,8 +27,7 @@ fun main()
           println("Докажите, что Вы не бот. Решите пример: $ranNum1 + $ranNum2 = ..")
           val userAnswer = readln()
 
-          if (userAnswer == sum)
-          {
+          if (userAnswer == sum) {
               println("Добро пожаловать!")
               return
           } else println("Ответ неверный!")
