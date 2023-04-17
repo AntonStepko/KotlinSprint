d51de88f1287cd2296e84ce8766f349e2876a554
AntonStepko <stepkoanton@gmail.com>
2023-04-17 18:01:39 +0300

    KS-9-2 print data from ninth lesson

---
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -0,0 +1,25 @@
+package lesson_9
+
+fun main() {
+
+    val list = mutableListOf("огурец", "помидор", "лук")
+
+    println("В рецепте есть базовые ингредиенты:")
+    list.forEach {
+        println(it)
+    }
+    println("Желаете добавить еще?")
+    val userAnswer = readln()
+
+    if (userAnswer == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        val newIngrid = readln()
+        list.add(newIngrid)
+
+        println("Теперь в рецепте есть следующие ингредиенты:")
+
+        list.forEach {
+            println(it)
+        }
+    } else false
+}
