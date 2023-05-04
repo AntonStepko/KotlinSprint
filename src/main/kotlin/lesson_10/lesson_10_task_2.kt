b441173e72cd4ecc7c7ba85343f8c2d9c9be5b8b
AntonStepko <stepkoanton@gmail.com>
2023-05-04 17:15:20 +0300

    KS-10-2 print data from tenth lesson

--- src/main/kotlin/lesson_10/lesson_10_task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_2.kt
@@ -1,16 +1,19 @@
 package lesson_10
 
 fun main() {
-    lengthPassword()
-}
-
-fun lengthPassword() {
     println("Введите логин:")
-    val login = readln()
+    val userLogin = readln()
     println("Введите пароль:")
-    val password = readln()
+    val userPassword = readln()
+    val result = checkingPasswordLength(userLogin, userPassword)
 
-    if ((login.length < 4) || (password.length < 4)) {
+    if (result) {
         println("Логин или пароль недостаточно длинные")
     } else println("Добро пожаловать!")
 }
+
+fun checkingPasswordLength (login: String, password: String): Boolean {
+    return (login.length < 4) || (password.length < 4)
+}
+
+
