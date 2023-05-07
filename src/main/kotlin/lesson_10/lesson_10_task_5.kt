cc2a20925c920bece788eed4dc57a1de9618b3e7
AntonStepko <stepkoanton@gmail.com>
2023-05-07 19:06:28 +0300

    KS-10-5 print data from tenth lesson

---
+++ src/main/kotlin/lesson_10/lesson_10_task_5.kt
@@ -0,0 +1,52 @@
+package lesson_10
+
+fun main() {
+    println("Придумайте логин (длина должна быть более 4 символов):")
+    do {
+        val userLogin = readln()
+        val result = checkLoginLength(userLogin)
+        if (!result) {
+            println("Логин недостаточно длинный")
+            Thread.sleep(1500)
+        }
+    } while (!result)
+
+    val generatedPassword = generatePassword()
+    println("Ваш пароль: $generatedPassword")
+    Thread.sleep(1500)
+
+    println("Введите логин и пароль:")
+    var userLogin = readln()
+    var userPassword = readln()
+
+    while ((userLogin != userLogin) || (userPassword != generatedPassword)) {
+        println("Неверный логин или пароль, введите заново:")
+        userLogin = readln()
+        userPassword = readln()
+    }
+
+    do {
+        val sms = generateSms()
+        println("Ваш код: $sms")
+        Thread.sleep(1500)
+        println("Введите код:")
+        val userSms = readln().toInt()
+    } while (userSms != sms)
+
+    println("Вы успешно авторизовались!")
+
+}
+
+fun checkLoginLength(login: String): Boolean {
+    return login.length >= 4
+}
+
+fun generatePassword(): String {
+    val data = ('a'..'z') + ('A'..'Z') + (0..9)
+    val lengthPassword = 6
+    return List(lengthPassword) { data.random() }.joinToString("")
+}
+
+fun generateSms(): Int {
+    return (1000..9999).random()
+}
