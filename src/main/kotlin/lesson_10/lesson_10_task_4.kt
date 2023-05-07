891abe959a872a2f33496ba9654f47a1f6719716
AntonStepko <stepkoanton@gmail.com>
2023-05-07 14:44:25 +0300

    KS-10-4 print data from tenth lesson

---
+++ src/main/kotlin/lesson_10/lesson_10_task_4.kt
@@ -0,0 +1,39 @@
+package lesson_10
+
+fun main() {
+    var round = 0
+
+    do {
+        val (cubeOfHuman1, cubeOfHuman2) = throwRandomize()
+        val (cubeOfComp1, cubeOfComp2) = throwRandomize()
+        val humanResult = cubeOfHuman1 + cubeOfHuman2
+        val compResult = cubeOfComp1 + cubeOfComp2
+        println("Ваш бросок:")
+        println("Ваш результат $cubeOfHuman1 и $cubeOfHuman2 \n")
+        Thread.sleep(1500)
+
+        println("Бросок компьютера:")
+        println("Результат компьютера $cubeOfComp1 и $cubeOfComp2 \n")
+        Thread.sleep(1500)
+
+        if (humanResult > compResult) {
+            round++
+            println("Победил мешок с костями! \n")
+        } else if (humanResult == compResult) {
+            println("Ничья!")
+        } else println("Победила бездушная машина! \n")
+
+        println("Хотите бросить кости еще раз Введите Да или Нет?")
+        val userAnswer = readln()
+
+    } while (userAnswer == "да")
+
+    println("Вы выиграли $round партий.")
+}
+
+fun throwRandomize(): Pair<Int, Int> {
+
+    val cube1 = (1..6).random()
+    val cube2 = (1..6).random()
+    return Pair(cube1, cube2)
+}
