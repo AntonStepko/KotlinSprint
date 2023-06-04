59bd0bb4b1dab0cf8e386e680c9c80e7711febcb
AntonStepko <stepkoanton@gmail.com>
2023-06-04 22:44:11 +0300

    KS-11-3 print data from eleventh lesson

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -1,10 +1,14 @@
 package lesson_11
 
-class Room (
-        val cover: Int,
-        val title: String,
-        val avatar: List<String>,
-        val nickname: List<String>,
+class Room(
+    val cover: Int,
+    val title: String,
+)
+
+class Member(
+    val id: Int,
+    val avatar: List<String>,
+    val nickname: List<String>,
 ) {
 
     fun speakStatus() {
@@ -12,26 +16,33 @@ class Room (
     }
 
     fun getList(): List<String> =
-            avatar.zip(nickname) {avatar, nickname -> "$avatar - $nickname"}
-
+        avatar.zip(nickname) { avatar, nickname -> "$avatar - $nickname" }
 }
-fun main () {
+
+fun main() {
 
     val room1 = Room(
         cover = 1,
         title = "Чтение книг",
-        avatar = listOf("Лев", "Тигр", "Смайл"),
-        nickname = listOf("Сергей", "Дмитрий", "Игорь"),
     )
 
     val room2 = Room(
         cover = 2,
         title = "Ремонт авто",
+    )
+
+    val member1 = Member(
+        id = 1,
+        avatar = listOf("Лев", "Тигр", "Смайл"),
+        nickname = listOf("Сергей", "Дмитрий", "Игорь"),
+    )
+
+    val member2 = Member(
+        id = 2,
         avatar = listOf("Луна", "Город", "Кошка"),
         nickname = listOf("Алексей", "Петр", "Елена"),
     )
 
-    val index = 2
-    println(room1.getList()[index])
-    room1.speakStatus()
+    println(member1.getList()[2])
+    member1.speakStatus()
 }
